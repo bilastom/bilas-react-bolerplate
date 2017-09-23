e31dfce9ac4c6c3facbab70cc6ea41b6527df16d
Tomasz Biłas <tomasz@bilas.pl>
2017-09-23 17:52:57 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../stylesheets/App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Bilas React Bolerplate
+      </div>
+    );
+  }
+}
+
+export default App;
